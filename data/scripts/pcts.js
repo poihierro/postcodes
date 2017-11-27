f7795560e38fca2df1aaa415111d6a8448b2462f
Poi Hierro <thierro@expedia.com>
2017-11-27 23:38:43 +0800

    to removed

--- data/scripts/pcts.js
+++ data/scripts/pcts.js
@@ -1,28 +1,28 @@
-"use strict";
+// "use strict";
 
-const {extract} = require("./index");
+// const {extract} = require("./index");
 
-/**
- * @module DataParser/pcts
- *
- * Writes pcts.json to stdout
- */
+// /**
+//  * @module DataParser/pcts
+//  *
+//  * Writes pcts.json to stdout
+//  */
 
-const CODE_OFFSET = 0;
-const VALUE_OFFSET = 2;
+// const CODE_OFFSET = 0;
+// const VALUE_OFFSET = 2;
 
-const transform = row => {
-	const code = row[CODE_OFFSET];
-	const value = row[VALUE_OFFSET];
-	if (code === "PCTCD") return []; // Escape if header
-	return [code, value];
-};
+// const transform = row => {
+// 	const code = row[CODE_OFFSET];
+// 	const value = row[VALUE_OFFSET];
+// 	if (code === "PCTCD") return []; // Escape if header
+// 	return [code, value];
+// };
 
-const configs = [
-	{
-		file: "PCT names and codes UK as at 12_16.txt",
-		transform
-	}
-];
+// const configs = [
+// 	{
+// 		file: "PCT names and codes UK as at 12_16.txt",
+// 		transform
+// 	}
+// ];
 
-extract({ configs });
+// extract({ configs });
